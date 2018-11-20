ae796646a90f74fecc1960c368b9977c08e0d278
Marc MacLeod <marbemac@gmail.com>
2018-11-20 11:43:08 -0500

    feat: add pointer helpers (#4)
    * feat: add pointer helpers
    
    These are extracted and cleaned up from the json-ref-parser repo. The are generically applicable to everywhere we want to work with json pointers (graph, ui-tree, studio, etc).
    
    * refactor(pointerToPath): prefer dynamic array
    
    * refactor(pointerToPath): ReferenceError to URIError

---
+++ src/__tests__/jsonPointerToPath.ts
@@ -0,0 +1,11 @@
+import { pointerToPath } from '../pointerToPath';
+
+test('pointerToPath', () => {
+  expect(pointerToPath('#/foo')).toEqual(['foo']);
+  expect(pointerToPath('#/foo/bar')).toEqual(['foo', 'bar']);
+  expect(pointerToPath('#/0')).toEqual(['0']);
+  expect(pointerToPath('#/paths/~1users')).toEqual(['paths', '/users']);
+  expect(pointerToPath('#/paths/foo~0users')).toEqual(['paths', 'foo~users']);
+  expect(pointerToPath('#')).toEqual([]);
+  expect(pointerToPath('#/')).toEqual(['']);
+});
