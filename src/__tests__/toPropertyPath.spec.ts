78d5a01ffc88c1a346217b237c723be29ccb6aa2
Karol Maciaszek <karol@maciaszek.pl>
2019-03-11 11:47:15 +0100

    fix: handling dots-in-path case (#11)
    * fix: handling dots-in-path case
    
    * fix: covered dotted and quoted case
    
    * fix: making linter happy
    
    * fix: moved sanitizing to separate function

--- src/__tests__/toPropertyPath.spec.ts
+++ src/__tests__/toPropertyPath.spec.ts
@@ -7,4 +7,7 @@ test('toPropertyPath', () => {
   expect(toPropertyPath('/paths/~1~1~1~1pets/post')).toEqual('paths.////pets.post');
   expect(toPropertyPath('/paths/foo~0users')).toEqual('paths.foo~users');
   expect(toPropertyPath('#/definitions/Error')).toEqual('definitions.Error');
+  expect(toPropertyPath('/a/..dotted/path')).toEqual('a.["..dotted"].path');
+  expect(toPropertyPath('/a/..dotted/path')).toEqual('a.["..dotted"].path');
+  expect(toPropertyPath('/a/..dotted.and."quoted"/path')).toEqual('a.["..dotted.and.\\"quoted\\""].path');
 });
--- src/toPropertyPath.ts
+++ src/toPropertyPath.ts
@@ -5,5 +5,14 @@ export function toPropertyPath(path: string) {
     .replace(/^(\/|#\/)/, '')
     .split('/')
     .map(decodePointerFragment)
+    .map(sanitize)
     .join('.');
+}
+
+function sanitize(fragment: string) {
+  if (fragment.includes('.')) {
+    return `["${fragment.replace(/"/g, '\\"')}"]`;
+  } else {
+    return fragment;
+  }
 }
