ed70f90b18fb78eb8c51a76fb4fd577483ffcbab
Jakub Rożek <P0lip@users.noreply.github.com>
2019-03-12 17:31:31 +0100

    feat: jsonc-parser [SL-1937] (#10)
    * feat: switch to jsonc-parser
    
    * feat: error handling
    
    * chore: bump Node in CircleCI config
    
    * fix: handle array properties positions
    
    * feat: add columns to error position
    
    * fix: support arrays
    
    * chore: tslint warnings
    
    * test: cover trailing commas + comments
    
    * chore: new lines
    
    * refactor: remove rest arguments
    
    * chore: explain why

--- src/__tests__/parseWithPointers.ts
+++ src/__tests__/parseWithPointers.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+import { join } from 'path';
 import { parseWithPointers } from '../parseWithPointers';
 
 describe('json parser', () => {
@@ -17,5 +19,112 @@ describe('json parser', () => {
   }
 }`)
     ).toMatchSnapshot();
+  });
+
+  test('parse complex', () => {
+    expect(
+      parseWithPointers(`{
+  "users": [
+    {
+      "name": "Markku",
+      "address": {
+         "country": "Finland",
+         "city": "Helsinki"
+       },
+      "age": 17,
+      "adult": false
+    },
+    {
+      "name": "Dennis",
+      "country": "Denmark",
+      "age": 18,
+      "adult": true
+    }
+  ]
+}`)
+    ).toMatchSnapshot();
+  });
+
+  test('does not allow comments by default', () => {
+    expect(
+      parseWithPointers(`{
+      // a comment
+      "name": "Antti",
+      /* a
+       very
+       block 
+       comment  
+       */
+      "city": "Vantaa"
+    }`)
+    ).toHaveProperty('validations', [
+      expect.objectContaining({
+        name: 'InvalidCommentToken',
+        location: expect.objectContaining({
+          start: expect.objectContaining({
+            line: 2,
+          }),
+        }),
+      }),
+      expect.objectContaining({
+        name: 'InvalidCommentToken',
+        location: expect.objectContaining({
+          start: expect.objectContaining({
+            line: 4,
+          }),
+        }),
+      }),
+    ]);
+  });
+
+  test('does not allow trailing commas by default', () => {
+    expect(
+      parseWithPointers(`{
+      "name": "Antti",
+       "city": "Vantaa",
+    }`)
+    ).toHaveProperty('validations', [
+      expect.objectContaining({
+        name: 'PropertyNameExpected',
+        location: {
+          start: expect.objectContaining({
+            line: 4,
+          }),
+          end: expect.objectContaining({
+            line: 4,
+          }),
+        },
+      }),
+      expect.objectContaining({
+        name: 'ValueExpected',
+        location: {
+          start: expect.objectContaining({
+            line: 4,
+          }),
+          end: expect.objectContaining({
+            line: 4,
+          }),
+        },
+      }),
+    ]);
+  });
+
+  describe('fixtures', () => {
+    it.each(['main.oas2.json', 'user.jschema.json'])('parses %s', async filename => {
+      expect(
+        parseWithPointers((await fs.promises.readFile(join(__dirname, 'fixtures', filename), 'utf-8')) as string)
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe('invalid fixtures', () => {
+    it.each(['schema.json', 'characters.json'])('parses %s', async filename => {
+      expect(
+        parseWithPointers((await fs.promises.readFile(
+          join(__dirname, 'fixtures/invalid', filename),
+          'utf-8'
+        )) as string)
+      ).toMatchSnapshot();
+    });
   });
 });
