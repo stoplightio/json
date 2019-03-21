e3f3ef057c1faf359f9b9636cc42ffe28ccd1f97
Marc MacLeod <marbemac@gmail.com>
2019-03-20 22:25:13 -0500

    chore: de-yalc types package

--- .yalc/@stoplight/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./http-spec"), exports);
-tslib_1.__exportStar(require("./logs"), exports);
-tslib_1.__exportStar(require("./diagnostics"), exports);
-//# sourceMappingURL=index.js.map
--- .yalc/@stoplight/types/schemas.d.ts
+++
@@ -1,3 +0,0 @@
-export interface ISchema {
-    [key: string]: any;
-}
