b206f3d5f7f9d6cbd8b67d31e96ee8583422db60
Jakub Rożek <jakub@stoplight.io>
2019-06-17 09:05:12 +0800

    fix: try casting string indexes

--- src/getLocationForJsonPath.ts
+++ src/getLocationForJsonPath.ts
@@ -17,7 +17,8 @@ export const getLocationForJsonPath: GetLocationForJsonPath<IJsonASTNode, number
 
 // based on source code of https://github.com/microsoft/node-jsonc-parser
 function findNodeAtPath(node: IJsonASTNode, path: JsonPath, closest: boolean): IJsonASTNode | undefined {
-  pathLoop: for (const segment of path) {
+  pathLoop: for (const part of path) {
+    const segment = Number.isNaN(Number(part)) ? part : Number(part);
     if (typeof segment === 'string') {
       if (node.type !== 'object' || !Array.isArray(node.children)) {
         return closest ? node : void 0;
