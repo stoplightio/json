5cc7d2635e63605753aeb5263ed4a69470f968fe
Thomas Pytleski <pytlesk4@gmail.com>
2020-05-22 17:24:26 -0500

    fix: only bundle local refs

--- src/__tests__/bundle.spec.ts
+++ src/__tests__/bundle.spec.ts
@@ -59,4 +59,64 @@ describe('bundleTargetPath()', () => {
       },
     });
   });
+
+  it('should not throw erorr', () => {
+    const document = {
+      definitions: {
+        user: {
+          id: 'foo',
+          address: {
+            $ref: '#/definitions/address',
+          },
+        },
+        address: {
+          street: 'foo',
+          user: {
+            $ref: '#/definitions/user',
+          },
+        },
+      },
+      __target__: {
+        entity: {
+          $ref: '#/definitions/user',
+        },
+        entity2: {
+          $ref: './path/to/pet.json',
+        },
+      },
+    };
+
+    const clone = cloneDeep(document);
+
+    const result = bundleTarget({
+      document: clone,
+      path: '#/__target__',
+    });
+
+    // Do not mutate document
+    expect(clone).toEqual(document);
+
+    expect(result).toEqual({
+      entity: {
+        $ref: '#/definitions/user',
+      },
+      entity2: {
+        $ref: './path/to/pet.json',
+      },
+      definitions: {
+        user: {
+          id: 'foo',
+          address: {
+            $ref: '#/definitions/address',
+          },
+        },
+        address: {
+          street: 'foo',
+          user: {
+            $ref: '#/definitions/user',
+          },
+        },
+      },
+    });
+  });
 });
