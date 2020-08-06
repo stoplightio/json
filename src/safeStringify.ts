d08decfa03e4e5ee295d15703b6cd16c509abfc5
Jakub Rożek <jakub@stoplight.io>
2020-08-06 19:29:02 +0200

    fix: improve safeStringify fn signature (#60)
    * fix: improve safeStringify fn signature
    
    * chore: disallow bigints
    
    * chore: cover toJSON
    
    * chore: cover toJSON more correctly
    
    * test: some extra ones + revert big int change
    
    * chore: void

--- src/safeStringify.ts
+++ src/safeStringify.ts
@@ -3,11 +3,19 @@ import { Optional } from '@stoplight/types';
 // Be very careful if considering switching out the underlying library to a different one!
 import fastStringify from 'safe-stable-stringify';
 
-export const safeStringify = (
-  value: any,
-  replacer?: (key: string, value: any) => any | Array<number | string> | null,
+type Replacer = (key: string, value: any) => any | Array<number | string> | null;
+
+type Voidable = undefined | Function | symbol | void;
+type Serializable = string | boolean | number | object | null;
+
+export function safeStringify(value: Voidable, replacer?: Replacer, space?: string | number): undefined;
+export function safeStringify<T extends object>(
+  value: T,
+  replacer?: Replacer,
   space?: string | number,
-): Optional<string> => {
+): T extends { toJSON(): infer R } ? (R extends Voidable ? undefined : string) : string;
+export function safeStringify(value: Serializable, replacer?: Replacer, space?: string | number): string;
+export function safeStringify(value: unknown, replacer?: Replacer, space?: string | number): Optional<string> {
   if (typeof value === 'string') {
     return value;
   }
@@ -19,4 +27,4 @@ export const safeStringify = (
   } catch {
     return fastStringify(value, replacer, space);
   }
-};
+}
