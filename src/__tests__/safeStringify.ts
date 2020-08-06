a7a037d760da8d830553fc7a4e180c9885787bda
Marcell Toth <marcell@marcelltoth.net>
2020-08-06 20:37:28 +0200

    Revert "fix: improve safeStringify fn signature (#60)"
    This reverts commit d08decfa03e4e5ee295d15703b6cd16c509abfc5.

--- src/__tests__/safeStringify.ts
+++ src/__tests__/safeStringify.ts
@@ -47,44 +47,8 @@ describe('safeStringify', () => {
     expect(safeStringify([0, 1])).toEqual('[0,1]');
   });
 
-  it('should not serialize undefined, symbols and functions', () => {
-    expect(safeStringify(Symbol('d'))).toBeUndefined();
-    expect(safeStringify(Function)).toBeUndefined();
+  it('should not serialize undefined', () => {
     expect(safeStringify(undefined)).toBeUndefined();
-  });
-
-  it('should cover toJSON', () => {
-    expect(safeStringify({ toJSON: () => 2 })).toEqual('2');
-    expect(safeStringify({ toJSON: () => null })).toEqual('null');
-    expect(safeStringify({ toJSON: () => undefined })).toBeUndefined();
-    expect(
-      safeStringify({
-        toJSON: () => {
-          /* nada */
-        },
-      }),
-    ).toBeUndefined();
-    expect(safeStringify({ toJSON: () => Symbol('d') })).toBeUndefined();
-    expect(safeStringify({ toJSON: () => Function })).toBeUndefined();
-    expect(
-      safeStringify({
-        a: undefined,
-        toJSON() {
-          return this.a;
-        },
-      }),
-    ).toBeUndefined();
-    expect(
-      safeStringify({
-        toJSON() {
-          if (Function('return Math.random() > 1')()) {
-            return 2;
-          }
-
-          return;
-        },
-      }),
-    ).toBeUndefined();
   });
 
   it('should handle falsy values correctly', () => {
