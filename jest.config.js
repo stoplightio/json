f58124eecee008cb101214b88fb434ec0a578a57
Jakub Rożek <jakub@stoplight.io>
2020-08-09 22:02:51 +0200

    feat: introduce stringify function (#63)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   preset: '@stoplight/scripts',
+  testEnvironment: 'node'
 };
---
+++ src/__tests__/stringify.spec.ts
@@ -0,0 +1,24 @@
+import { safeStringify } from '../safeStringify';
+import { stringify } from '../stringify';
+
+describe('stringify', () => {
+  it('should produce the same result as safeStringify', () => {
+    expect(stringify({ foo: true })).toEqual(safeStringify({ foo: true }));
+
+    expect(stringify({ foo: true, bar: false }, void 0, 4)).toEqual(
+      safeStringify(
+        {
+          foo: true,
+          bar: false,
+        },
+        void 0,
+        4,
+      ),
+    );
+  });
+
+  it.each([void 0, Symbol(), Function])('should throw for unserializable %s value', value => {
+    expect(stringify.bind(null, value)).toThrow();
+    expect(stringify.bind(null, { toJSON: () => value })).toThrow();
+  });
+});
