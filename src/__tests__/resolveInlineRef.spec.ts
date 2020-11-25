b6c2d482b32e08fda559251c1b96ccc5c17b5b9f
Jakub Rożek <jakub@stoplight.io>
2020-11-25 16:12:28 +0200

    feat: implement resolveInlineRef util

---
+++ src/__tests__/resolveInlineRef.spec.ts
@@ -0,0 +1,114 @@
+import { resolveInlineRef } from '../resolveInlineRef';
+
+describe('resolveInlineRef', () => {
+  test('should follow refs', () => {
+    const doc = {
+      a: {
+        $ref: '#/b/foo',
+      },
+      b: {
+        $ref: '#/c',
+      },
+      c: {
+        $ref: '#/d',
+      },
+      d: {
+        foo: 'woo!',
+      },
+    };
+
+    expect(resolveInlineRef(doc, '#/a')).toEqual('woo!');
+  });
+
+  test('should handle direct circular refs', () => {
+    const doc = {
+      a: {
+        $ref: '#/b',
+      },
+      b: {
+        $ref: '#/a',
+      },
+    };
+
+    expect(resolveInlineRef(doc, '#/a')).toEqual({
+      $ref: '#/b',
+    });
+  });
+
+  test('should handle direct circular refs #2', () => {
+    const doc = {
+      a: {
+        $ref: '#/b/foo',
+      },
+      b: {
+        foo: {
+          $ref: '#/c',
+        },
+        bar: {
+          $ref: '#/e',
+        },
+      },
+      c: {
+        $ref: '#/b/bar',
+      },
+      e: {
+        $ref: '#/a',
+      },
+    };
+
+    expect(resolveInlineRef(doc, '#/a')).toEqual({
+      bar: {
+        $ref: '#/e',
+      },
+      foo: {
+        $ref: '#/c',
+      },
+    });
+  });
+
+  test('given external reference, should throw', () => {
+    const doc = {
+      a: {
+        $ref: './foo#/b/foo',
+      },
+    };
+
+    expect(resolveInlineRef.bind(null, doc, '#/a')).toThrowError('Cannot resolve external references');
+  });
+
+  test('given missing segment, should throw', () => {
+    const doc = {
+      a: {
+        $ref: '#/b/foo',
+      },
+      b: {
+        bar: false,
+      },
+    };
+
+    expect(resolveInlineRef.bind(null, doc, '#/a')).toThrowError("Could not resolve '#/b/foo'");
+  });
+
+  test('given path pointing at invalid data, should throw', () => {
+    const doc = {
+      a: {
+        $ref: '#/b/foo/bar',
+      },
+      b: {
+        foo: null,
+      },
+    };
+
+    expect(resolveInlineRef.bind(null, doc, '#/a')).toThrowError("Could not resolve '#/b/foo/bar'");
+  });
+
+  test('given invalid $ref type, should throw', () => {
+    const doc = {
+      a: {
+        $ref: 2,
+      },
+    };
+
+    expect(resolveInlineRef.bind(null, doc, '#/a')).toThrowError('$ref should be a string');
+  });
+});
