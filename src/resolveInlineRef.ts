ff6daa47a587606676dafff2efc1002d600efb05
Thomas Pytleski <pytlesk4@gmail.com>
2021-05-04 15:12:56 -0500

    feat: add decycle method (#81)
    * feat: add decycle method
    
    - blocking for https://github.com/stoplightio/platform-internal/pull/6180
    - find circular javascript references and replace them with a JSON Ref
    
    * feat: a bit of type-safety
    
    * chore: re-export
    
    Co-authored-by: Jakub Rożek <jakub@stoplight.io>

--- src/resolveInlineRef.ts
+++ src/resolveInlineRef.ts
@@ -1,10 +1,7 @@
 import { Dictionary } from '@stoplight/types';
+import { isPlainObject } from './_utils';
 import { extractSourceFromRef } from './extractSourceFromRef';
 import { pointerToPath } from './pointerToPath';
-
-function isObject(maybeObj: unknown): maybeObj is { [key in PropertyKey]: unknown } {
-  return typeof maybeObj === 'object' && maybeObj !== null;
-}
 
 function _resolveInlineRef(document: Dictionary<unknown>, ref: string, seen: unknown[]): unknown {
   const source = extractSourceFromRef(ref);
@@ -15,13 +12,13 @@ function _resolveInlineRef(document: Dictionary<unknown>, ref: string, seen: unk
   const path = pointerToPath(ref);
   let value: unknown = document;
   for (const segment of path) {
-    if (!isObject(value) || !(segment in value)) {
+    if (!isPlainObject(value) || !(segment in value)) {
       throw new ReferenceError(`Could not resolve '${ref}'`);
     }
 
     value = value[segment];
 
-    if (isObject(value) && '$ref' in value) {
+    if (isPlainObject(value) && '$ref' in value) {
       if (seen.includes(value)) {
         // circular, let's stop
         return seen[seen.length - 1];
@@ -43,7 +40,7 @@ function _resolveInlineRef(document: Dictionary<unknown>, ref: string, seen: unk
 
   const sourceDocument = seen[seen.length - 1];
 
-  if (isObject(sourceDocument) && ('summary' in sourceDocument || 'description' in sourceDocument)) {
+  if (isPlainObject(sourceDocument) && ('summary' in sourceDocument || 'description' in sourceDocument)) {
     return {
       ...value,
       ...('description' in sourceDocument ? { description: sourceDocument.description } : null),
