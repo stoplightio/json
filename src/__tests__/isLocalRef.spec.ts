867da5ba694c55920da41f260fb59425d9cd7e42
Vazha Omanashvili <rainum@users.noreply.github.com>
2021-05-17 17:06:14 +0300

    fix: local refs should not contain any whitespace characters (#89)
    * fix: local refs should not contain any whitespace characters
    
    * chore: # edge case and tests
    
    * fix: make regex less strict

--- src/__tests__/isLocalRef.spec.ts
+++ src/__tests__/isLocalRef.spec.ts
@@ -1,11 +1,39 @@
 import { isLocalRef } from '../isLocalRef';
 
 describe('isLocalRef', () => {
-  it.each(['#', '#/', '#/foo', '#/0/1', '#/~1'])('should treat %s as local reference', ref => {
+  it.each([
+    '#',
+    '#/foo',
+    '#/foo/0',
+    '#/',
+    '#/a~1b',
+    '#/c%25d',
+    '#/e%5Ef',
+    '#/g%7Ch',
+    '#/i%5Cj',
+    '#/k%22l',
+    '#/%20',
+    '#/m~0n',
+    '#./d~8z',
+  ])('should treat %s as local reference', ref => {
     expect(isLocalRef(ref)).toEqual(true);
   });
 
-  it.each(['', '../#', 'http://a#', '/foo/#', '../test.yaml#/'])('should not treat %s as local reference', ref => {
-    expect(isLocalRef(ref)).toEqual(false);
+  it.each([
+    '',
+    '../#',
+    'http://a#',
+    '/foo/#',
+    '../test.yaml#/',
+    '#/foo\n/bar',
+    '#/foo\t/bar',
+    '#/foo\u2006​/bar',
+    ' #/foo/bar',
+    '#/foo /bar',
+    '#/foo/bar ',
+    '# Hello, world!',
+    'foo',
+  ])('should not treat %s as local reference', ref => {
+    expect(isLocalRef(ref as string)).toEqual(false);
   });
 });
