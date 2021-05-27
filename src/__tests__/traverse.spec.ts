a700fe5733d40ce620f3866174cd978ea22c0804
Jakub Rożek <jakub@stoplight.io>
2021-05-27 16:51:11 +0300

    feat(traverse): support onEnter/onLeave

--- src/__tests__/traverse.spec.ts
+++ src/__tests__/traverse.spec.ts
@@ -20,4 +20,39 @@ describe('traverse', () => {
     traverse(obj, mockFunc);
     expect(mockFunc).toHaveBeenCalledTimes(0);
   });
+
+  it('should support onEnter/onLeave hooks', () => {
+    const obj = {
+      foo: {
+        bar: 'bear',
+      },
+    };
+
+    const onEnter = jest.fn();
+    const onLeave = jest.fn();
+    traverse(obj, {
+      onEnter,
+      onLeave,
+    });
+
+    expect(onEnter).toHaveBeenCalledTimes(2);
+    expect(onEnter).nthCalledWith(1, {
+      path: [],
+      value: obj,
+    });
+    expect(onEnter).nthCalledWith(2, {
+      path: ['foo'],
+      value: obj.foo,
+    });
+
+    expect(onLeave).toHaveBeenCalledTimes(2);
+    expect(onLeave).nthCalledWith(1, {
+      path: ['foo'],
+      value: obj.foo,
+    });
+    expect(onLeave).nthCalledWith(2, {
+      path: [],
+      value: obj,
+    });
+  });
 });
