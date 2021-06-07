574422be6daad8deb9240d0fd9c1d3afc032e669
Jakub Rożek <jakub@stoplight.io>
2021-06-07 12:20:54 +0200

    feat: expose isPlainObject util (#92)

---
+++ src/__tests__/isPlainObject.spec.ts
@@ -0,0 +1,13 @@
+import { isPlainObject } from '../isPlainObject';
+
+it('isPlainObject', () => {
+  expect(isPlainObject(new class Foo {}())).toBe(false);
+  expect(isPlainObject([1, 2, 3])).toBe(false);
+
+  expect(isPlainObject({ x: 0, y: 0 })).toBe(true);
+  expect(isPlainObject(Object.create(null))).toBe(true);
+
+  const a = new class {}();
+  a.constructor = Object;
+  expect(isPlainObject(a)).toBe(true);
+});
