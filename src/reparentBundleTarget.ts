4d8b3efcad13e2e44ad6fdfd3ba8f74b85a7c192
Jakub Rożek <jakub@stoplight.io>
2021-08-10 23:21:09 +0200

    chore: add a comment

--- src/reparentBundleTarget.ts
+++ src/reparentBundleTarget.ts
@@ -6,6 +6,40 @@ function isObject(maybeObj: unknown): maybeObj is Record<string, unknown> | unkn
   return isPlainObject(maybeObj) || Array.isArray(maybeObj);
 }
 
+/**
+ * reparentBundleTarget - the function provides a way to change the main root of all $refs.
+ * To illustrate the example, let's say you have a JSON Schema Draft 7 model that uses "definitions" and you'd like to move all these $refs to "$defs"
+ * {
+ *  "type": "object",
+ *  "properties": {
+ *    "user": {
+ *      "$ref": "#/definitions/User"
+ *    }
+ *  },
+ *  "definitions": {
+ *    "User": {
+ *      "type": "object"
+ *    }
+ *   }
+ * }
+ * reparentBundleTarget(document, '#/definitions', '#/$defs'); // this **MUTATES** the data, so make sure to make a copy of it if you don't want your data to be lost
+ * {
+ *  "type": "object"
+ *  "properties": {
+ *    "user": {
+ *      "$ref": "#/$defs/User"
+ *    }
+ *  },
+ *  "$defs": {
+ *    "User": {
+ *     "type": "object"
+ *   }
+ * }
+ *
+ * @param document - the input document, i.e. a JSON Schema model, or a OAS document
+ * @param from - the root to move from
+ * @param to - the root to migrate to
+ */
 export function reparentBundleTarget(document: Record<string, unknown>, from: string, to: string): void {
   if (to.length <= 1 || from.length <= 1) {
     throw Error('Source/target path must not be empty and point at root');
