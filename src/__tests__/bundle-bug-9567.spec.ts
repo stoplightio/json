07774cc13cc4bc8dea4497e1a1d8bf5bd35469de
William Hilton <wmhilton@gmail.com>
2022-01-24 19:00:12 -0500

    fix: yarn lint.fix

--- src/__tests__/bundle-bug-9567.spec.ts
+++ src/__tests__/bundle-bug-9567.spec.ts
@@ -1,18 +1,18 @@
-import { cloneDeep } from "lodash";
+import { cloneDeep } from 'lodash';
 
-import { BUNDLE_ROOT as BUNDLE_ROOT_POINTER, bundleTarget } from "../bundle";
+import { BUNDLE_ROOT as BUNDLE_ROOT_POINTER, bundleTarget } from '../bundle';
 
-import * as document from "./fixtures/bundle-bug-9567.json";
+import * as document from './fixtures/bundle-bug-9567.json';
 
 const BUNDLE_ROOT = BUNDLE_ROOT_POINTER.slice(2);
 
-describe("bundleTarget()", () => {
-  it("should handle refs in refs", () => {
+describe('bundleTarget()', () => {
+  it('should handle refs in refs', () => {
     const clone = cloneDeep(document);
 
     const result = bundleTarget({
       document: clone,
-      path: "#/__target__",
+      path: '#/__target__',
     });
 
     // Do not mutate document
@@ -22,18 +22,18 @@ describe("bundleTarget()", () => {
     expect(Array.isArray(result[BUNDLE_ROOT])).toBe(false);
 
     expect(result[BUNDLE_ROOT]).toHaveProperty(
-      "0",
+      '0',
       expect.objectContaining({
         properties: {
           user_id: {
-            format: "uuid",
-            type: "string",
+            format: 'uuid',
+            type: 'string',
           },
         },
-        required: ["user_id"],
-        title: "user_reference",
-        type: "object",
-      })
+        required: ['user_id'],
+        title: 'user_reference',
+        type: 'object',
+      }),
     );
   });
 });
