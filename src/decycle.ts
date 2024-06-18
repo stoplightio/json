f629a386d4e2e0d58529e1e1e1001ff44d0579c2
sb-rohitdesai <rohitkumar.desai@smartbear.com>
2024-06-18 12:50:27 +0530

    fix(dereference): added comments

--- src/decycle.ts
+++ src/decycle.ts
@@ -9,14 +9,17 @@ export function decycle(obj: unknown, replacer?: (value: any) => any) {
     }
 
     if (isPlainObject(value) || Array.isArray(value)) {
+      // The path of an earlier occurance of value
       const oldPath = objs.get(value);
 
+      // If the value is an object or array, look to see if we have already
+      // encountered it. If so, return a {"$ref":PATH} object.
       if (oldPath) {
         return { $ref: oldPath };
       }
 
       objs.set(value, pathToPointer(path));
-
+      // If it is an array, replicate the array.
       if (Array.isArray(value)) {
         return value.map((element, i) => derez(element, [...path, i]));
       }
