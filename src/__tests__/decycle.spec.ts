79de4db22ff0ea8a650f50651e28cbaab39ac401
Bhaskar_Sontakke <bhaskar_sontakke@psl-lv1wgfmggy.persistent.co.in>
2024-07-22 15:41:55 +0530

    Revert "fix(dereference): modification for circular ref for delete objects (#132)"
    This reverts commit ccea3e3058971e65d424ac2eeea840828a255de4.

--- src/__tests__/decycle.spec.ts
+++ src/__tests__/decycle.spec.ts
@@ -105,7 +105,7 @@ describe('decycle', () => {
     });
   });
 
-  it('should create refs circular objects and repeated values', () => {
+  it('should not create refs for objects that are not circular', () => {
     const obj2 = {
       circle: {},
     };
@@ -131,7 +131,7 @@ describe('decycle', () => {
         foo: 'bar',
       },
       obj3B: {
-        $ref: '#/obj3A',
+        foo: 'bar',
       },
       paths: {
         '/circle': {
