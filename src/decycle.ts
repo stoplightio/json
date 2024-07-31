fa24856a7031d7f16ccae28203a5c1cebbdb85e7
SB-rohitdesai <165134671+SB-rohitdesai@users.noreply.github.com>
2024-07-31 12:56:35 +0530

    fix: Optimised code for performance (#142)

--- src/decycle.ts
+++ src/decycle.ts
@@ -3,39 +3,38 @@ import { pathToPointer } from './pathToPointer';
 
 export function decycle(obj: unknown, replacer?: (value: any) => any) {
   const objs = new WeakMap<object, string>();
+  const processedObjs = new WeakSet<object>();
   function derez(value: any, path: (string | number)[]): any {
     if (replacer) {
       value = replacer(value);
     }
-
     if (isPlainObject(value) || Array.isArray(value)) {
       // The path of an earlier occurance of value
       const oldPath = objs.get(value);
-
       // If the value is an object or array, look to see if we have already
       // encountered it. If so, return a {"$ref":PATH} object.
       if (oldPath) {
         return { $ref: oldPath };
       }
-
       objs.set(value, pathToPointer(path));
       // If it is an array, replicate the array.
       if (Array.isArray(value)) {
         return value.map((element, i) => derez(element, [...path, i]));
       }
-
       const newObj: Record<string, any> = {};
       for (const name in value) {
         if (Object.prototype.hasOwnProperty.call(value, name)) {
           newObj[name] = derez(value[name], [...path, name]);
         }
       }
-      // deleteing object before returing
-      objs.delete(value);
+      // Only delete the object from the map if it has not been processed before
+      if (!processedObjs.has(value)) {
+        objs.delete(value);
+      }
+      processedObjs.add(value);
       return newObj;
     }
     return value;
   }
-
   return derez(obj, []);
 }
