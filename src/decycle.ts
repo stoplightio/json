a8a81f71cfb658014ea4bb749afa597e1568cb00
SB-rohitdesai <165134671+SB-rohitdesai@users.noreply.github.com>
2024-08-02 12:29:31 +0530

    Fix/stop 184/performance issue (#143)
    * fix: Optimised code for performance
    
    * fix: changes in logic for resolve test case issue

--- src/decycle.ts
+++ src/decycle.ts
@@ -1,40 +1,41 @@
 import { isPlainObject } from './isPlainObject';
 import { pathToPointer } from './pathToPointer';
 
-export function decycle(obj: unknown, replacer?: (value: any) => any) {
+export const decycle = (obj: unknown, replacer?: (value: any) => any) => {
   const objs = new WeakMap<object, string>();
   const processedObjs = new WeakSet<object>();
-  function derez(value: any, path: (string | number)[]): any {
-    if (replacer) {
-      value = replacer(value);
-    }
+
+  return (function derez(value: any, path: string[]) {
+    // The new object or array
+    let curObj: any;
+
+    // If a replacer function was provided, then call it to get a replacement value.
+    if (replacer) value = replacer(value);
+
     if (isPlainObject(value) || Array.isArray(value)) {
       // The path of an earlier occurance of value
       const oldPath = objs.get(value);
       // If the value is an object or array, look to see if we have already
       // encountered it. If so, return a {"$ref":PATH} object.
-      if (oldPath) {
-        return { $ref: oldPath };
-      }
+      if (oldPath) return { $ref: oldPath };
+
       objs.set(value, pathToPointer(path));
       // If it is an array, replicate the array.
       if (Array.isArray(value)) {
-        return value.map((element, i) => derez(element, [...path, i]));
+        curObj = value.map((element, i) => derez(element, [...path, String(i)]));
+      } else {
+        // It is an object, replicate the object.
+        curObj = {};
+        Object.keys(value).forEach(name => {
+          curObj[name] = derez(value[name], [...path, name]);
+        });
       }
-      const newObj: Record<string, any> = {};
-      for (const name in value) {
-        if (Object.prototype.hasOwnProperty.call(value, name)) {
-          newObj[name] = derez(value[name], [...path, name]);
-        }
-      }
-      // Only delete the object from the map if it has not been processed before
       if (!processedObjs.has(value)) {
         objs.delete(value);
       }
       processedObjs.add(value);
-      return newObj;
+      return curObj;
     }
     return value;
-  }
-  return derez(obj, []);
-}
+  })(obj, []);
+};
